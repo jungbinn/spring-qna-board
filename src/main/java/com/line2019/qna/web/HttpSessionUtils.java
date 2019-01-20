9352481374bc70655d21760862a1301e94f28dac
Jungbinn Park <wjdqls7958@kaist.ac.kr>
2019-01-20 17:38:42 +0900

    로그인, 로그아웃 기능 구현 (#2)
    * Add login, logout features
    
    - HttpSession login, logout
    - 로그인시 화면에 user name을 보여줌
    
    * Add list page
    
    - Show all user list

---
+++ src/main/java/com/line2019/qna/web/HttpSessionUtils.java
@@ -0,0 +1,6 @@
+package com.line2019.qna.web;
+
+public class HttpSessionUtils {
+    public static final String SESSION_KEY = "session-user";
+
+}
--- src/main/java/com/line2019/qna/web/UserController.java
+++ src/main/java/com/line2019/qna/web/UserController.java
@@ -2,10 +2,13 @@ package com.line2019.qna.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/users")
@@ -23,5 +26,35 @@ public class UserController {
     public String create(@ModelAttribute User user) {
         userRepository.save(user);
         return "redirect:/";
+    }
+
+    @GetMapping("loginForm")
+    public String loginForm() {
+        return "users/login";
+    }
+
+    @PostMapping("login")
+    public String login(String email, String password, HttpSession session) {
+        User user = userRepository.findByEmail(email);
+        if (!user.getPassword().equals(password)) {
+            System.out.printf("Login Fail for user %s: password %s != %s\n", user.getEmail(), user.getPassword(), password);
+            return "redirect:/users/loginForm";
+        }
+
+        session.setAttribute(HttpSessionUtils.SESSION_KEY, user);
+        System.out.printf("Login Success %s", user.getEmail());
+        return "redirect:/";
+    }
+
+    @GetMapping("logout")
+    public String logout(HttpSession session) {
+        session.removeAttribute(HttpSessionUtils.SESSION_KEY);
+        return "redirect:/";
+    }
+
+    @GetMapping("")
+    public String list(Model model) {
+        model.addAttribute("users", userRepository.findAll());
+        return "users/list";
     }
 }
