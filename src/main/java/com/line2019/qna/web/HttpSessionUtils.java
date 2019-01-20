a2d819b01c4274b3220b41040cdcbe3421e21c46
jungbinn <wjdqls7958@kaist.ac.kr>
2019-01-20 18:01:01 +0900

    Add user data update feature

--- src/main/java/com/line2019/qna/web/HttpSessionUtils.java
+++ src/main/java/com/line2019/qna/web/HttpSessionUtils.java
@@ -1,6 +1,19 @@
 package com.line2019.qna.web;
 
+import javax.servlet.http.HttpSession;
+
 public class HttpSessionUtils {
     public static final String SESSION_KEY = "session-user";
 
+    public static User getSessionUser(HttpSession session) {
+        return (User) session.getAttribute(HttpSessionUtils.SESSION_KEY);
+    }
+
+    public static boolean isUserLogin(HttpSession session) {
+        return getSessionUser(session) != null;
+    }
+
+    public static boolean isUserAdmin(HttpSession session) {
+        return isUserLogin(session) && getSessionUser(session).isAdmin();
+    }
 }
