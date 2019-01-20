e806a2cc517cf026a8880fa31f8ec82367f3f958
jungbinn <wjdqls7958@kaist.ac.kr>
2019-01-20 15:20:45 +0900

    Apply livereload

--- src/main/java/com/line2019/qna/QnaApplication.java
+++ src/main/java/com/line2019/qna/QnaApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class QnaApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("spirng.devtools.restart.enabled", "false");
+		System.setProperty("spirng.devtools.livereload.enabled", "true");
 		SpringApplication.run(QnaApplication.class, args);
 	}
 
